66dc3f7be904ca8e280da1192369c8417df019ba
fawad kaleem <fwdkaleem@gmail.com>
2023-11-22 02:19:37 +0500

    Frebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:hikkingfinder_web/add_event.dart';
 import 'package:hikkingfinder_web/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
